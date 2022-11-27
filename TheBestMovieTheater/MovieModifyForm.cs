ef6afda3bf7c182507a1cdb5c9705961f5354f59
Zach <LE2189542@crc-lennox.qc.ca>
2022-11-27 03:01:35 -0500

    StyleCop Dealt with.

--- TheBestMovieTheater/MovieModifyForm.cs
+++ TheBestMovieTheater/MovieModifyForm.cs
@@ -1,30 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.Common;
-using System.Drawing;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Documents;
-using System.Windows.Forms;
+﻿// <copyright file="MovieModifyForm.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace TheBestMovieTheater
 {
+    using System;
+    using System.Data;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// MovieModifyForm contains the design and functions for manager options.
+    /// </summary>
     public partial class MovieModifyForm : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MovieModifyForm"/> class.
+        /// Default Constructor for MovieModifyForm.
+        /// </summary>
         public MovieModifyForm()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
+        /// <summary>
+        /// Closes the form on button click.
+        /// </summary>
+        /// <param name="sender">The button that was clicked.</param>
+        /// <param name="e">Additional event arguments.</param>
         private void BackButton_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        /// <summary>
+        /// Fills listview with data on form load.
+        /// </summary>
+        /// <param name="sender">The form that was loaded.</param>
+        /// <param name="e">Additional event arguments.</param>
         private void MovieModifyForm_Load(object sender, EventArgs e)
         {
             DataTable movie = this.movieTableAdapter.GetData();
